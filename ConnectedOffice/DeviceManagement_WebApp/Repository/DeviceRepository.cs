4efb517a25a1420f5613b1160475cf38c4c5e2ed
seymourpublic <naledisandamela@gmail.com>
2022-09-22 12:36:24 +0200

    added the device repo and its code

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -0,0 +1,26 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DeviceRepository
+    {
+            protected readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+
+            // GET ALL: Products
+            public IEnumerable<Device> GetAll()
+            {
+                return _context.Device.ToList();
+            }
+
+            // TO DO: Add ‘Get By Id’
+            // TO DO: Add ‘Create’
+            // TO DO: Add ‘Edit’
+            // TO DO: Add ‘Delete’
+            // TO DO: Add ‘Exists’
+        
+
+    }
+}
