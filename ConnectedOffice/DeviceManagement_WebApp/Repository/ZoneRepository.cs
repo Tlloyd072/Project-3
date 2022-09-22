56503c7e97291a059e586ac61d64da5a87077739
seymourpublic <naledisandamela@gmail.com>
2022-09-22 13:55:13 +0200

    created the zone repo and its code

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,26 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository
+    {
+        protected readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+
+        // GET ALL: Products
+        public IEnumerable<Zone> GetAll()
+        {
+            return _context.Zone.ToList();
+        }
+
+        // TO DO: Add ‘Get By Id’
+        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Edit’
+        // TO DO: Add ‘Delete’
+        // TO DO: Add ‘Exists’
+
+
+    }
+}
