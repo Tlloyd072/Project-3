e4bf1960346828c4977803c0075028d62979ac7b
seymourpublic <naledisandamela@gmail.com>
2022-09-23 16:29:08 +0200

    added the namespace to the repo

--- ConnectedOffice/DeviceManagement_WebApp/Repository/GenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/GenericRepository.cs
@@ -4,40 +4,44 @@ using System;
 using DeviceManagement_WebApp.Data;
 using System.Linq;
 
-public class GenericRepository<T> : IGenericRepository<T> where T : class
+namespace DeviceManagement_WebApp.Repository
 {
-    protected readonly ConnectedOfficeContext _context;
-    public GenericRepository(ConnectedOfficeContext context)
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        _context = context;
+        protected readonly ConnectedOfficeContext _context;
+        public GenericRepository(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
     }
-    public void Add(T entity)
-    {
-        _context.Set<T>().Add(entity);
-    }
-    public void AddRange(IEnumerable<T> entities)
-    {
-        _context.Set<T>().AddRange(entities);
-    }
-    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
-    {
-        return _context.Set<T>().Where(expression);
-    }
-    public IEnumerable<T> GetAll()
-    {
-        return _context.Set<T>().ToList();
-    }
-    public T GetById(int id)
-    {
-        return _context.Set<T>().Find(id);
-    }
-    public void Remove(T entity)
-    {
-        _context.Set<T>().Remove(entity);
-    }
-    public void RemoveRange(IEnumerable<T> entities)
-    {
-        _context.Set<T>().RemoveRange(entities);
-    }
+
 }
 
