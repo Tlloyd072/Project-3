d77b0295a53f7eff8f9729d2ffb4730151d96c75
seymourpublic <naledisandamela@gmail.com>
2022-09-23 16:29:35 +0200

    added the namespace to the repo

--- ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepositorye.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepositorye.cs
@@ -2,13 +2,19 @@
 using System.Linq.Expressions;
 using System;
 
-public interface IGenericRepository<T> where T : class
+
+namespace DeviceManagement_WebApp.Repository
 {
-    T GetById(int id);
-    IEnumerable<T> GetAll();
-    IEnumerable<T> Find(Expression<Func<T, bool>> expression);
-    void Add(T entity);
-    void AddRange(IEnumerable<T> entities);
-    void Remove(T entity);
-    void RemoveRange(IEnumerable<T> entities);
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
 }
+
+
