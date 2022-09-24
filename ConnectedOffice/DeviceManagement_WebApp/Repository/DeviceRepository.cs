e7176e7618ddac20b740f00aee54421f9c1689b6
seymourpublic <naledisandamela@gmail.com>
2022-09-24 13:04:48 +0200

    added three methods to be implemented on the device controller

--- ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -10,12 +10,47 @@ namespace DeviceManagement_WebApp.Repository
         {
 
         }
+
+        public bool DeleteDevice(int id)
+        {
+            bool result = false;
+            var dev = _context.Device.Find(id);
+            _context.Device.Remove(dev);
+            _context.SaveChanges();
+
+            result = true;
+
+            return result;
+            
+            
+        }
+
         public Device GetMostRecentService()
         {
             return _context.Device.OrderByDescending(Device => Device.DateCreated).FirstOrDefault();
         }
 
+        public bool SaveDevice(Device device)
+        {
+            bool outcome = false;
+            _context.Device.Add(device);
+            _context.SaveChanges();
+            outcome = true;
 
+
+            return outcome;
+        }
+
+        public bool UpdateDevice(Device device)
+        {
+            bool outcome = false;
+            _context.Device.Update(device);
+            _context.SaveChanges();
+            outcome = true;
+
+
+            return outcome;
+        }
     }
     
     
